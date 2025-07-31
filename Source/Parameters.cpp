4e8d1a568c951c8357ce5be727cc8cd246146069
AlexGrischenko <grischenko_a@yahoo.co.uk>
2025-07-31 22:52:11 +0200

    Added MIX and GAIN buttons with functionality

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -40,6 +40,21 @@ static juce::String stringFromMilliseconds(float value, int) {
 }
 
 
+static float millisecondsFromString(const juce::String& text) {
+
+    float value = text.getFloatValue();
+    if (!text.endsWithIgnoreCase("ms")) {
+    
+        if (text.endsWithIgnoreCase("s") || value < Parameters::minDelayTime) {
+        
+            return value * 1000.0f;
+        
+        }
+    
+    }
+    return value;
+}
+
 static juce::String stringFromDecibels(float value, int) {
     return juce::String(value, 1) + " dB";
 }
@@ -83,6 +98,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
         100.0f,
         juce::AudioParameterFloatAttributes()
         .withStringFromValueFunction(stringFromMilliseconds)
+        .withValueFromStringFunction(millisecondsFromString)
     ));
 
     layout.add(std::make_unique<juce::AudioParameterFloat>(
